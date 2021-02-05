40bc690a1239c685d6bdba6c0bc9491506b7ccea
amir1226 <amir1226@gmail.com>
2021-02-05 15:59:48 -0500

    Collisition detection first approach

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,14 +21,21 @@ async function connect(options) {
 
 }
 //--------------------------------
+
+
 class Bullet {
+  static bullet_list = []
+  static id = 0
   constructor(el, x = 0, y = 0, angle = 0) {
     this.el = el
     this.setState()
     this.setAngle(angle)
     this.setPosition(x, y)
     this.setVisibility(true)
-    this.setTim
+    Bullet.bullet_list.push(this)
+    Bullet.id += 1
+    this.radio = (document.getElementsByClassName(this.el.className)[0].width)/2
+
   }
 
   setState(go = 0, direction = 0) {
@@ -62,10 +69,18 @@ class Bullet {
   }
 
   setVisibility(visible) {
-    this.el.style.visibility = 'visible'
+    if (visible) {
+      this.el.style.visibility = 'visible'
+    }
+    else{
+      this.setState(0, 0)
+      document.getElementById(this.el.id).remove()
+      Bullet.bullet_list.splice(this.el.id -1,  1);
+    }
   }
 
   play() {
+    
     this.timer = setInterval(()=> {
       const { go, direction } = this.state 
 
@@ -78,13 +93,10 @@ class Bullet {
     }, 10)
   }
 
-  stop() {
-    clearInterval(this.timer)
-  }
-
   static create(parent, imagePath, x = 0, y = 0, angle = 0) {
     const img = document.createElement('img')
-    img.className = `bullet`
+    img.className = "bullet"
+    img.id = `${Bullet.id +1}`
     img.src = imagePath
     parent.appendChild(img)
     return new Bullet(img, x, y, angle)
@@ -92,13 +104,17 @@ class Bullet {
 }
 
 class StarShip {
+  
   constructor(el, x = 0, y = 0, angle = 0) {
     this.el = el
-
     this.setState()
     this.setAngle(angle)
     this.setPosition(x, y)
     this.setVisibility(true)
+
+    this.ship_width = document.getElementsByClassName(this.el.className)[0].width
+    this.ship_height = document.getElementsByClassName(this.el.className)[0].height
+    this.radio = this.ship_height/2
   }
 
   setState(go = 0, direction = 0) {
@@ -112,16 +128,14 @@ class StarShip {
 
   setPosition(x, y) {
 
-    const ship_width = document.getElementsByClassName(this.el.className)[0].width
-    const ship_height = document.getElementsByClassName(this.el.className)[0].height
     const window_width= document.getElementById('galaxy').clientWidth
     const window_height= document.getElementById('galaxy').clientHeight
 
-    if (x <= 0) x = window_width -(ship_width+1);
-    if (x +ship_width >= window_width) x = 0;
+    if (x <= 0) x = window_width -(this.ship_width+1);
+    if (x +this.ship_width >= window_width) x = 0;
 
-    if (y <= 0) y = window_height -(ship_height+1);
-    if (y +ship_height >= window_height) y=0;
+    if (y <= 0) y = window_height -(this.ship_height+1);
+    if (y +this.ship_height >= window_height) y=0;
     this.x = x
     this.y = y
 
@@ -147,6 +161,8 @@ class StarShip {
   }
 
   play() {
+    if(this.el.style.visibility === "hidden") return;
+    
     this.timer = setInterval(()=> {
       const { go, direction } = this.state  
       if (go === 0 && direction === 0) return;
@@ -155,15 +171,25 @@ class StarShip {
       const angle = (this.angle + direction) % 360
       const x = this.x + Math.sin(this.angle / 360.0 * 2 * Math.PI) * go
       const y = this.y - Math.cos(this.angle / 360.0 * 2 * Math.PI) * go
-  
+      
       this.setPosition(x, y)
       this.setAngle(angle)
-    }, 30)
+      this.detectBullet()
+    }, 15)
   }
 
-  stop() {
-    clearInterval(this.timer)
+  detectBullet(listBullets){
+    listBullets = Bullet.bullet_list;
+
+    listBullets.forEach(bullet => {
+      if (Math.hypot(bullet.x - this.x, bullet.y - this.y) < bullet.radio + this.radio){
+        console.log("Me dieron!!!!")
+        bullet.setVisibility(false)
+      } 
+    })
+    
   }
+
 
   static create(parent, imagePath, extraClass, x = 0, y = 0, angle = 0) {
     const img = document.createElement('img')
@@ -173,6 +199,8 @@ class StarShip {
     return new StarShip(img, x, y, angle)
   }
 }
+
+
 
 function addKeyEvent(batship) {
   const up = ['w', 'ArrowUp']
@@ -193,7 +221,8 @@ function addKeyEvent(batship) {
     if (stop.indexOf(e.key) >= 0) batship.setState(0, 0)
 
     if (space.indexOf(e.key) >= 0) {
-      const bullet = Bullet.create(galaxy, './assets/spaceship/bullet.png', batship.getX(), batship.getY(), batship.getAngle())
+      const bullet = Bullet.create(galaxy, './assets/spaceship/bullet.png', 
+      batship.getX(), batship.getY(), batship.getAngle())
       bullet.play()
       bullet.setState(1, 0)
     }
