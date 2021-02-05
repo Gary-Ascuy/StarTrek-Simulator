9ac010998bea6909db090566ad2f0575a4ed74a3
BayronCampaz <bayrondaymir@yahoo.es>
2021-02-05 12:21:38 -0500

    Organize files and implements of create room

---
+++ src/scripts/player.js
@@ -0,0 +1,14 @@
+class Player{
+    constructor(nickname = "", gender = "", starship = null, team = "") {
+        this.id = '_' + Math.random().toString(36).substr(2, 9);
+        this.nickname = nickname;
+        this.gender = gender;
+        this.team = team;
+    }
+
+    
+    setStartship(starship) {
+        this.starship = starship
+      }
+
+  }
