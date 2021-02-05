2ff8ab71ecbd6273d5b623e61a13a2e023b41d73
Jem Pool <jem.suarez@correounivalle.edu.co>
2021-02-05 09:58:52 -0500

    class scripts separated into modules

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,82 +4,18 @@ let idGamer = ""
 const rabbitmqSettings = {
   username: 'admin',
   password: 'admin',
-  host: 'frontend.ascuy.me',
-  port: 15675,
+  host: 'http://localhost/',
+  port: 15672,
   keepalive: 20,
   path: 'ws'
 }
-//--------------------------------
-class Bullet {
-  constructor(el, x = 0, y = 0, angle = 0) {
-    this.el = el
-
-    this.setState()
-    this.setAngle(angle)
-    this.setPosition(x, y)
-    this.setVisibility(true)
-  }
-
-  setState(go = 0, direction = 0) {
-    this.state = { go, direction }
-  }
-
-  setAngle(angle) {
-    this.angle = angle
-    this.el.style.transform = `rotate(${angle}deg)`
-  }
-
-  setPosition(x, y) {
-    this.x = x
-    this.y = y
-
-    this.el.style.left = `${x}px`
-    this.el.style.top = `${y}px`
-
-    // console.log("x: ", x, ", y ", y)
-  }
-
-  setVisibility(visible) {
-    this.el.style.visibility = visible ? 'visible' : 'hidden'
-  }
-
-  play() {
-    this.timer = setInterval(()=> {
-      const { go, direction } = this.state  
-      if (go === 0 && direction === 0) return;
-
-      const angle = (this.angle + direction) % 360
-      const x = this.x + Math.sin(this.angle / 360.0 * 2 * Math.PI) * go
-      const y = this.y - Math.cos(this.angle / 360.0 * 2 * Math.PI) * go
-  
-      this.setPosition(x, y)
-      this.setAngle(angle)
-    }, 0.3)
-  }
-
-  stop() {
-    clearInterval(this.timer)
-  }
-
-  static create(parent, imagePath, extraClass, x = 0, y = 0, angle = 0) {
-    const img = document.createElement('img')
-    // img.className = `starship ${extraClass}`
-    img.className = `bullet`
-    img.src = imagePath
-    parent.appendChild(img)
-
-    return new StarShip(img, x, y, angle)
-  }
-}
 
 function createBullet() {   
-  console.log('Creating a bullet')
   const bullet = StarShip.create(galaxy, './assets/spaceship/bullet.png', 'bullet', 0, 0, 45)
   bullet.play()  
   return bullet;
 }
 
-//--------------------------------
 async function connect(options) {
   try {
     const client = await RsupMQTT.connect(options)
@@ -88,79 +24,7 @@ async function connect(options) {
   } catch (error) {
     console.log(error)
   }
-}
 
-class StarShip {
-  constructor(el, x = 0, y = 0, angle = 0) {
-    this.el = el
-
-    this.setState()
-    this.setAngle(angle)
-    this.setPosition(x, y)
-    this.setVisibility(true)
-  }
-
-  setState(go = 0, direction = 0) {
-    this.state = { go, direction }
-  }
-
-  setAngle(angle) {
-    this.angle = angle
-    this.el.style.transform = `rotate(${angle}deg)`
-  }
-
-  setPosition(x, y) {
-    this.x = x
-    this.y = y
-
-    this.el.style.left = `${x}px`
-    this.el.style.top = `${y}px`
-
-    // console.log("x: ", x, ", y ", y)
-  }
-
-  getX(){
-    return this.x;
-  }
-
-  getY(){
-    return this.y;
-  }
-
-  getAngle(){
-    return this.angle;
-  }
-
-  setVisibility(visible) {
-    this.el.style.visibility = visible ? 'visible' : 'hidden'
-  }
-
-  play() {
-    this.timer = setInterval(()=> {
-      const { go, direction } = this.state  
-      if (go === 0 && direction === 0) return;
-
-      const angle = (this.angle + direction) % 360
-      const x = this.x + Math.sin(this.angle / 360.0 * 2 * Math.PI) * go
-      const y = this.y - Math.cos(this.angle / 360.0 * 2 * Math.PI) * go
-  
-      this.setPosition(x, y)
-      this.setAngle(angle)
-    }, 3)
-  }
-
-  stop() {
-    clearInterval(this.timer)
-  }
-
-  static create(parent, imagePath, extraClass, x = 0, y = 0, angle = 0) {
-    const img = document.createElement('img')
-    img.className = `starship ${extraClass}`
-    img.src = imagePath
-    parent.appendChild(img)
-
-    return new StarShip(img, x, y, angle)
-  }
 }
 
 function addKeyEvent(batship) {
@@ -182,7 +46,6 @@ function addKeyEvent(batship) {
     if (stop.indexOf(e.key) >= 0) batship.setState(0, 0)
 
     if (space.indexOf(e.key) >= 0) {
-      console.log("space")
       bullet = createBullet()
       bullet.setPosition(batship.getX(), batship.getY())
       bullet.setAngle(batship.getAngle())
@@ -200,19 +63,14 @@ async function main() {
   console.log('Starting Star Trek Simulator')
   const galaxy = document.getElementById('galaxy')
 
-  // console.log('Creating USS Enterprise element')
-  // const enterprise = StarShip.create(galaxy, './assets/spaceship/ussenterprise.png', 'ussenterprise', 0, 0, 90)
-  // enterprise.play()
-  // enterprise.setState(1, 0)
+  console.log('Creating USS Enterprise element')
+  const enterprise = StarShip.create(galaxy, './assets/spaceship/ussenterprise.png', 'ussenterprise', 1, 1, 90)
+  enterprise.play()
+  enterprise.setState(1, 0) 
 
-  const initialPosX = 0; //Math.ceil(Math.random()*400);
-  const initialPosY = 350; //Math.ceil(Math.random()*400);
-  const initialAngle = 0; //Math.ceil(Math.random()*360);
-  const batship = StarShip.create(galaxy, './assets/spaceship/batship.png', 'small batship', initialPosX, initialPosY, initialAngle)
+  const batship = StarShip.create(galaxy, './assets/spaceship/batship.png', 'small batship', 80, 300, 45)
   batship.play()
   addKeyEvent(batship)
-
-  document.getElementById('room').innerHTML = 'Room #' + room;
 
   console.log('Connecting to RabbitMQ/MQTT over WebSocket')
   await connect(rabbitmqSettings)
