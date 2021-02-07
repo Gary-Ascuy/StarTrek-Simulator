cbdfef96b92df16092f18c1bdce4d89b50185f5f
amir1226 <amir1226@gmail.com>
2021-02-07 17:42:34 -0500

    Detect Colition integration

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -50,8 +50,8 @@ async function connect(options) {
           }
           break;
         case "Ship movement":
-          console.log("Someone just moved..")
-          console.log(msj.id)
+  /*         console.log("Someone just moved..")
+          console.log(msj.id) */
 
           if(msj.id != ID) {
             ships[msj.id].setPosition(msj.x, msj.y)
@@ -70,107 +70,6 @@ async function connect(options) {
   }
 
 }
-
-
-
-
-class StarShip {
-  
-  constructor(el, x = 0, y = 0, angle = 0) {
-    this.el = el
-    this.setState()
-    this.setAngle(angle)
-    this.setPosition(x, y)
-    this.setVisibility(true)
-
-    this.ship_width = document.getElementsByClassName(this.el.className)[0].width
-    this.ship_height = document.getElementsByClassName(this.el.className)[0].height
-    this.radio = this.ship_height/2
-  }
-
-  setState(go = 0, direction = 0) {
-    this.state = { go, direction }
-  }
-
-  setAngle(angle) {
-    this.angle = angle
-    this.el.style.transform = `rotate(${angle}deg)`
-  }
-
-  setPosition(x, y) {
-
-    const window_width= document.getElementById('galaxy').clientWidth
-    const window_height= document.getElementById('galaxy').clientHeight
-
-    if (x <= 0) x = window_width -(this.ship_width+1);
-    if (x +this.ship_width >= window_width) x = 0;
-
-    if (y <= 0) y = window_height -(this.ship_height+1);
-    if (y +this.ship_height >= window_height) y=0;
-    this.x = x
-    this.y = y
-
-    this.el.style.left = `${x}px`
-    this.el.style.top = `${y}px`
-
-  }
-
-  getX(){
-    return this.x;
-  }
-
-  getY(){
-    return this.y;
-  }
-
-  getAngle(){
-    return this.angle;
-  }
-
-  setVisibility(visible) {
-    this.el.style.visibility = visible ? 'visible' : 'hidden'
-  }
-
-  play() {
-    if(this.el.style.visibility === "hidden") return;
-    
-    this.timer = setInterval(()=> {
-      const { go, direction } = this.state  
-      if (go === 0 && direction === 0) return;
-
-
-      const angle = (this.angle + direction) % 360
-      const x = this.x + Math.sin(this.angle / 360.0 * 2 * Math.PI) * go
-      const y = this.y - Math.cos(this.angle / 360.0 * 2 * Math.PI) * go
-      
-      this.setPosition(x, y)
-      this.setAngle(angle)
-      this.detectBullet()
-    }, 15)
-  }
-
-  detectBullet(listBullets){
-    listBullets = Bullet.bullet_list;
-
-    listBullets.forEach(bullet => {
-      if (Math.hypot(bullet.x - this.x, bullet.y - this.y) < bullet.radio + this.radio){
-        console.log("Me dieron!!!!")
-        bullet.setVisibility(false)
-      } 
-    })
-    
-  }
-
-
-  static create(parent, imagePath, extraClass, x = 0, y = 0, angle = 0) {
-    const img = document.createElement('img')
-    img.className = `starship ${extraClass}`
-    img.src = imagePath
-    parent.appendChild(img)
-    return new StarShip(img, x, y, angle)
-  }
-}
-
 
 
 function addKeyEvent(batship) {
@@ -192,10 +91,6 @@ function addKeyEvent(batship) {
     if (stop.indexOf(e.key) >= 0) batship.setState(0, 0)
 
     if (space.indexOf(e.key) >= 0) {
-      const bullet = Bullet.create(galaxy, './assets/spaceship/bullet.png', 
-      batship.getX(), batship.getY(), batship.getAngle())
-      bullet.play()
-      bullet.setState(1, 0)
 
       const bulletId = Date.now()
       const bullet = Bullet.create(galaxy, './assets/spaceship/bullet.png', 
@@ -219,13 +114,13 @@ async function main() {
   console.log('Connecting to RabbitMQ/MQTT over WebSocket')
   client = await connect(rabbitmqSettings)
   
-  // console.log('Creating USS Enterprise element')
-  // const enterprise = StarShip.create(galaxy, './assets/spaceship/ussenterprise.png', 'ussenterprise', 1, 1, 90)
-  // enterprise.play()
-  // enterprise.setState(1, 0) 
+  console.log('Creating USS Enterprise element')
+  const enterprise = StarShip.create(galaxy, SPRITEPATH, 'small batship', 10, 10, 90)
+  enterprise.play()
+  enterprise.setState(1, 0) 
+
 
   const batship = StarShip.create(galaxy, SPRITEPATH, 'small batship', 200, 200, 45)
-
   batship.play()
   addKeyEvent(batship)
 
